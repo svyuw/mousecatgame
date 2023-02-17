a9e0a80866d9d69e63242093be0ca6c027208511
svyuw <76976595+svyuw@users.noreply.github.com>
2023-02-17 15:38:18 -0800

    added mouse

--- index.js
+++ index.js
@@ -1,69 +1,57 @@
-// let cat;
-// let score = 0;
-
-// function preload() {
-//     catFrames = loadGif('cat.gif');
-// }
-
-// window.setup = () => {
-//   createCanvas(windowWidth, windowHeight);
-//   colorMode(RGB);
-//   background(193, 225, 193);
-//   cat = createSprite(width / 2, height / 2, 40, 40);
-//   cat.addAnimation('default', catFrames.frames);
-// }
-
-// window.draw = () => {
-
-// function keyPressed() {
-//     if (keyCode === (UP_ARROW)) {
-//            s.dir(0,-1);
-//         } else if (keyCode === (DOWN_ARROW)) {
-//             s.dir(0,1);
-//         } else if (keyCode === (RIGHT_ARROW)) {
-//             s.dir(1,0);
-//         } else if (eyCode === (LEFT_ARROW)) {
-//             s.dir(-1,0);
-//         }
-// }
-
-// function cat(){
-//     this.x = 0;
-//     this.y = 0; 
-//     this.xspeed = 1; 
-//     this.yspeed = 0; 
-// }
-
-// this.dir = function(x,y) {
-//     this.xspeed = x; 
-//     this.yspeed = y; 
-// }
-
-// drawSprites();
-
-// }
-
 let cat;
-let catFrames;
 let catLoad;
+let mouse = [];
+let mouseLoad;
+let score = 0;
 
 function preload() {
     catLoad = loadImage('cat.gif');
-    catFrames = createImg('cat.gif');
+    mouseLoad = loadImage('mouse.gif');
 }
 
-window.setup = () => {
+function setup () {
   createCanvas(windowWidth, windowHeight);
-  background(50);
-//   cat = createSprite(width / 2, height / 2, 40, 40);
-//   cat.addAnimation('default', catFrames);
+  cat = createSprite(width, height, 5, 5);
+  cat.addAnimation('default', catLoad);
 
 };
 
-window.draw = () => {
-//   cat.position.x = mouseX;
-//   cat.position.y = mouseY;
-    image(catLoad);  
-    catFrames.position(50, 350);
-//   drawSprites();
+function draw () {
+    colorMode(RGB);
+    background(193, 225, 193);
+
+    for (let mouseLoad of mice) {
+        mouseLoad.update();
+        if (cat.collide(mouseLoad.sprite)) {
+          mouseLoad.handleCollision();
+        }
+      }
+      drawSprite(cat);
+      text(`Score: ${score}`, 10, 20);
+
+
+    drawSprites();
+
+}
+
+function keyPressed() {
+    if (key === "ArrowUp") {
+      cat.velocity.y = -5;
+    } else if (key === "ArrowDown") {
+      cat.velocity.y = 5;
+    } else if (key === "ArrowLeft") {
+      cat.velocity.x = -5;
+    } else if (key === "ArrowRight") {
+      cat.velocity.x = 5;
+    }
 };
+
+function keyReleased() {
+    if (key === "ArrowUp" || key === "ArrowDown") {
+      cat.velocity.y = 0;
+    } else if (key === "ArrowLeft" || key === "ArrowRight") {
+      cat.velocity.x = 0;
+    }
+}
+
+
