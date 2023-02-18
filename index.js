04343bc1b4643d6d060afe5e946a2d27343b5e3e
svyuw <76976595+svyuw@users.noreply.github.com>
2023-02-17 17:46:09 -0800

    mice and cat move

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let cat;
 let catLoad;
-let mouse = [];
+let mice = [];
 let mouseLoad;
 let score = 0;
 
@@ -8,29 +8,48 @@ function preload() {
     catLoad = loadImage('cat.gif');
     mouseLoad = loadImage('mouse.gif');
 }
+  
 
 function setup () {
   createCanvas(windowWidth, windowHeight);
   cat = createSprite(width, height, 5, 5);
   cat.addAnimation('default', catLoad);
 
-};
+  for (let i = 0; i < 20; i++) {
+    let mouse = {
+      x: random(width),
+      y: random(height),
+      size: 60,
+      vx: random(-2, 2),
+      vy: random(-2, 2)
+    };
+    mice.push(mouse);
+  }
+
+}
 
 function draw () {
     colorMode(RGB);
     background(193, 225, 193);
-
-    for (let mouseLoad of mice) {
-        mouseLoad.update();
-        if (cat.collide(mouseLoad.sprite)) {
-          mouseLoad.handleCollision();
+    
+    for (let i = 0; i < mice.length; i++) {
+      
+        mice[i].vx += random(-0.1, 0.1);
+        mice[i].vy += random(-0.1, 0.1);
+    
+        let speed = sqrt(mice[i].vx * mice[i].vx + mice[i].vy * mice[i].vy);
+        if (speed > 5) {
+          mice[i].vx *= 5 / speed;
+          mice[i].vy *= 5 / speed;
         }
+
+        mice[i].x += mice[i].vx;
+        mice[i].y += mice[i].vy;
+   
+        image(mouseLoad, mice[i].x, mice[i].y, mice[i].size, mice[i].size);
       }
-      drawSprite(cat);
-      text(`Score: ${score}`, 10, 20);
 
-
-    drawSprites();
+      drawSprites();
 
 }
 
@@ -53,5 +72,4 @@ function keyReleased() {
       cat.velocity.x = 0;
     }
 }
-
 
